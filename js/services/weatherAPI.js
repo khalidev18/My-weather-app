78dbff3b9e972bd2a67d08cffb52d18aee874953
khalifa <khalidev99@gmail.com>
2024-10-29 06:55:23 -0700

    there was a mistake with the api

--- js/services/weatherAPI.js
+++ js/services/weatherAPI.js
@@ -1,12 +1,14 @@
 export default class WeatherAPI {
     constructor() {
-        this.API_KEY = import.meta.env.VITE_WEATHER_API_KEY || 'bd5e378503939ddaee76f12ad7a97608';
+        this.API_KEY = 'bd5e378503939ddaee76f12ad7a97608';
         this.BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
     }
 
     async getWeather(city) {
         const response = await fetch(
-            `${this.BASE_URL}?q=${city}&appid=${this.API_KEY}&units=metric`
+            `https://api.allorigins.win/raw?url=${encodeURIComponent(
+                `${this.BASE_URL}?q=${city}&appid=${this.API_KEY}&units=metric`
+            )}`
         );
 
         if (!response.ok) {
